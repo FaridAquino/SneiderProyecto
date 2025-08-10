abbae2cd31603d26ee2bbbe6bddea36e50372c1e
FaridAquino <farid.aquino@utec.edu.pe>
2025-08-09 23:37:29 -0500

    first commit

---
+++ src/main/java/com/example/TarmaLimpia/TarmaLimpiaApplication.java
@@ -0,0 +1,13 @@
+package com.example.TarmaLimpia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TarmaLimpiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TarmaLimpiaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/TarmaLimpia/TarmaLimpiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TarmaLimpia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TarmaLimpiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
